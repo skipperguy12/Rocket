8779dcb977fc8b68d55df22bf41f43b26b61ccfc
mrapple <tony@oc.tc>
2013-05-27 21:25:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Rocket
+==============
+
+Plugin for rocketing players
+
+Permissions
+===========
+
+`rocket.rocket`
+
+Allow the user to left click a player and rocket them
---
+++ src/main/java/tc/oc/rocket/RocketPlugin.java
@@ -0,0 +1,14 @@
+package tc.oc.rocket;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RocketPlugin extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+    }
+
+    @Override
+    public void onEnable() {
+    }
+}
